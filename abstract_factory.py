9049bb0905d64e43e8d272d3a272a3ad4b26286f
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-02 23:38:04 -0300

    More methods
    Create methods for the affiliates

--- abstract_factory.py
+++ abstract_factory.py
@@ -13,3 +13,33 @@ class Pizzafactory(ABC):
     @abstractmethod
     def createpizzatraditional(self):
         pass
+
+class Regionrich(Pizzafactory):
+    def createpizzaveg(self):
+        return Typevegrich()
+
+    def createpizzacandy(self):
+        return Typecandyrich()
+
+    def createpizzatraditional(self):
+        return Typetraditionalrich()
+
+class Regionpoor(Pizzafactory):
+    def createpizzaveg(self):
+        return Typevegpoor()
+
+    def createpizzacandy(self):
+        return Typecandypoor()
+
+    def createpizzatraditional(self):
+        return Typetraditionalpoor()
+
+class Regionmediun(Pizzafactory):
+    def createpizzaveg(self):
+        return Typevegmediun()
+
+    def createpizzacandy(self):
+        return Typecandymediun()
+
+    def createpizzatraditional(self):
+        return Typetraditionalmediun()
