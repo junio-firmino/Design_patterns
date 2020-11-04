fd1f8db2ca264a132fff64fbaffd73287dc85159
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-04 14:49:41 -0300

    Finished the code.

--- abstract_factory.py
+++ abstract_factory.py
@@ -1,93 +1,134 @@
 from abc import ABC, abstractmethod
 
 
+'''Pizza Factory'''
+
+
 class Pizzafactory(ABC):
     def __init__(self):
-        self.createpizzaveg()
-        self.createpizzacandy()
+        self.optionpizzaveg()
+        self.optionpizzacandy()
+        self.optionpizzatraditional()
 
     @abstractmethod
-    def createpizzaveg(self):
+    def optionpizzaveg(self):
         pass
 
     @abstractmethod
-    def createpizzacandy(self):
+    def optionpizzacandy(self):
         pass
 
     @abstractmethod
-    def createpizzatraditional(self):
+    def optionpizzatraditional(self):
         pass
 
-class Regionrich(Pizzafactory):         #Lojas nas regiões
-    def createpizzaveg(self):
+
+''' Lojas nas Regiões '''
+
+
+class Regionrich(Pizzafactory):
+    def optionpizzaveg(self):
         return Typevegrich().descricion()
 
-    def createpizzacandy(self):
+    def optionpizzacandy(self):
         return Typecandyrich().descricion()
 
-    def createpizzatraditional(self):
-        return Typetraditionalrich()
+    def optionpizzatraditional(self):
+        return Typetraditionalrich().descricion()
+
 
 class Regionpoor(Pizzafactory):
-    def createpizzaveg(self):
+    def optionpizzaveg(self):
         return Typevegpoor().descricion()
 
-    def createpizzacandy(self):
+    def optionpizzacandy(self):
         return Typecandypoor().descricion()
 
-    def createpizzatraditional(self):
-        return Typetraditionalpoor()
+    def optionpizzatraditional(self):
+        return Typetraditionalpoor().descricion()
+
 
 class Regionmediun(Pizzafactory):
-    def createpizzaveg(self):
+    def optionpizzaveg(self):
         return Typevegmediun().descricion()
 
-    def createpizzacandy(self):
+    def optionpizzacandy(self):
         return Typecandymediun().descricion()
 
-    def createpizzatraditional(self):
-        return Typetraditionalmediun()
+    def optionpizzatraditional(self):
+        return Typetraditionalmediun().descricion()
+
+
+'''Tipos de pizzas'''
+
 
 class Vegpizza(ABC):
     @abstractmethod
     def descricion(self):
         pass
 
+
 class Typevegrich(Vegpizza):
     def descricion(self):
-        print('Massa VEGANA a base dos melhores ingredientes.')
+        print('Massa VEGANA a base dos melhores ingredientes, Público classe A.')
+
 
 class Typevegpoor(Vegpizza):
     def descricion(self):
-        print('Massa VEGANA a base de ingredientes reaproveitados.')
+        print('Massa VEGANA a base de ingredientes reaproveitados, Público classe C.')
+
 
 class Typevegmediun(Vegpizza):
     def descricion(self):
-        print('Massa VEGANA a base de ingredientes de 2 linha.')
+        print('Massa VEGANA a base de ingredientes de 2 linha, Público em GERAL.')
+
 
 class Candypizza(ABC):
     def descricion(self):
         pass
 
+
 class Typecandyrich(Candypizza):
     def descricion(self):
-        print('Massa DOCE a base dos melhores ingredientes.')
+        print('Massa DOCE a base dos melhores ingredientes, Público classe A.')
+
 
 class Typecandypoor(Candypizza):
     def descricion(self):
-        print('Massa DOCE a base de produtos reaproveitados.')
+        print('Massa DOCE a base de produtos reaproveitados, Público classe C.')
+
 
 class Typecandymediun(Candypizza):
     def descricion(self):
-        print('Massa DOCE a base de produtos de 2 linha.')
+        print('Massa DOCE a base de produtos de 2 linha, Público em GERAL.')
 
 
+class Traditional(ABC):
+    @abstractmethod
+    def descricion(self):
+        pass
 
 
+class Typetraditionalrich(Traditional):
+    def descricion(self):
+        print('Massa TRADICIONAL a base dos melhores ingredientes, Público classe A.')
 
 
+class Typetraditionalpoor(Traditional):
+    def descricion(self):
+        print('Massa TRADICIONAL a base de ingredientes reaproveitados, Público classe C.')
 
+
+class Typetraditionalmediun(Traditional):
+    def descricion(self):
+        print('Massa TRADICIONAL a base de ingredientes de 2 linha, Público em GERAL.')
 
 
 if __name__ == '__main__':
-    ff = Regionrich()
+    tipos_escolhas = input('Qual a zona da região você mora? ').title()
+    if tipos_escolhas == 'Rica':
+        rr = Regionrich()
+    elif tipos_escolhas == 'Pobre':
+        pp = Regionpoor()
+    else:
+        mm = Regionmediun()
