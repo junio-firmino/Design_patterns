af2a6866b51cf9fc919fe6c5e8427449a2f6ce46
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-07 23:32:05 -0300

    Begin undestand Facade Design Patterns.

---
+++ Facade.py
@@ -0,0 +1,9 @@
+class Gerenciamentoevento():
+    def __init__(self):
+        print('Vamos conversar sobre o evento?.')
+
+    def organization(self):
+        self.hotelier = Hotelier()
+        self.hotelier.bookhotel()
+
+
