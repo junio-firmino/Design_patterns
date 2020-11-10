af3a20f6c2f84c68404156469adb64e5dd00e809
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-09 23:06:35 -0300

    New organization
    New organization the class python.

--- Facade.py
+++ Facade.py
@@ -6,10 +6,14 @@ class Gerenciamentoevento():
         self.hotelier = Hotelier()
         self.hotelier.bookhotel()
 
+    def organization_presentation(self):
         self.florist = Florist()
         self.florist.setflowerrequirements()
 
     def organization_musicians(self):
-        self
+        self.musician = Musician()
+        self.musician.setmusictype()
+
+    
 
 
