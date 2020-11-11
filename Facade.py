9ede68367e5c9f991fc46d0bd3003ef7ff36758a
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-11 00:10:36 -0300

    Finished The code.

--- Facade.py
+++ Facade.py
@@ -1,6 +1,6 @@
-class Gerenciamentoevento():
+class Gerenciamentoevento:
     def __init__(self):
-        print('Vamos conversar sobre o evento?.')
+        print('Vamos conversar sobre o que foi executado do evento até o momento?.')
 
     def organization_initial(self):
         self.hotelier = Hotelier()
@@ -18,20 +18,49 @@ class Gerenciamentoevento():
         self.buffet = Buffet()
         self.buffet.setbuffet()
 
+
 class Hotelier:
     def bookhotel(self):
         print('Reservas dos quartos providencidos!.')
+
 
 class Florist:
     def setflowerrequirements(self):
         print('Tipos de flores e aromas providenciados!.')
 
+
 class Musician:
     def setmusictype(self):
         print('Musicos e repertórios escolhidos e providenciados!.')
+
 
 class Buffet:
     def setbuffet(self):
         print('Tipos de aperitivos, comidas e doces escolhidos e providenciados!')
 
 
+class Interface:
+    def __init__(self):
+        print('Vamos organizar o casamento?')
+
+    def askinterface(self,x):
+        ge = Gerenciamentoevento()
+        if x == 'Hotel':
+            ge.organization_initial()
+        elif x == 'Flores':
+            ge.organization_presentation()
+        elif x == 'Musical':
+            ge.organization_musicians()
+        elif x == 'Buffet':
+            ge.organization_buffet()
+        else:
+            ge.organization_initial()
+            ge.organization_presentation()
+            ge.organization_musicians()
+            ge.organization_buffet()
+
+
+if __name__ == '__main__':
+    x = input('Qual parte da organização do casamento o Senhor quer acompanhar?.').title()
+    inter = Interface()
+    inter.askinterface(x)
