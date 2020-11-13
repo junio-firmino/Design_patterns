d562a21cd7b2de5fbcf976f9c302d4bc3c68a6e1
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-13 00:11:02 -0300

    Solution patterns Proxy

--- Proxy.py
+++ Proxy.py
@@ -1,16 +1,33 @@
 class Actor:
-    def __init__(self):
-        self.occuped = False
+    def __init__(self,nm):
+        if nm == 'True':
+            self.occuped = True  # Retorna  OCUPADO
+        else:
+            self.occuped = False  # Retorna DISPONÍVEL
 
     def occupied(self):
-        self.occuped = True
-        print(type().__name__,'está ocupado com novo filme.')
+        #self.occuped = True
+        print(type(self).__name__,'está ocupado com novo filme.')
 
     def available(self):
-        self.occuped = False
-        print(type().__name__,'está disponível para novos trabalhos.')
+        #self.occuped = False
+        print(type(self).__name__,'está disponível para novos trabalhos.')
 
     def getstatus(self):
         return self.occuped
 
-    
+class Agent:
+    def __init__(self):
+        self.sd = None
+
+    def work(self):
+        es = input("False ou True? ").title()
+        self.actor = Actor(es)
+        if self.actor.getstatus():
+            self.actor.occupied()
+        else:
+            self.actor.available()
+
+if __name__ == '__main__':
+    r=Agent()
+    r.work()
