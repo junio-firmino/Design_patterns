949c13d2a218357d5c221d5b20571df9c2e33ea0
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-11-15 23:36:19 -0300

    Organization the code.

--- Proxy.py
+++ Proxy.py
@@ -1,33 +1,37 @@
 class Actor:
-    def __init__(self,nm):
+    def __init__(self, nm):
         if nm == 'True':
             self.occuped = True  # Retorna  OCUPADO
         else:
             self.occuped = False  # Retorna DISPONÍVEL
 
     def occupied(self):
-        #self.occuped = True
-        print(type(self).__name__,'está ocupado com novo filme.')
+        # self.occuped = True
+        print(type(self).__name__, 'está ocupado com novo filme.')
 
     def available(self):
-        #self.occuped = False
-        print(type(self).__name__,'está disponível para novos trabalhos.')
+        # self.occuped = False
+        print(type(self).__name__, 'está disponível para novos trabalhos.')
 
     def getstatus(self):
         return self.occuped
 
+
 class Agent:
     def __init__(self):
-        self.sd = None
+        self.actor = None
 
     def work(self):
         es = input("False ou True? ").title()
         self.actor = Actor(es)
-        if self.actor.getstatus():  # Se o método getstatus for TRUE o método continua.
-            self.actor.occupied()   # No entanto se o método for FALSE o mesmo não prossegue e vai para outra parte da decisão
+        if self.actor.getstatus():
+            # Se o método getstatus for TRUE o método continua,no entanto,
+            # se o método for FALSE o mesmo não prossegue e vai para outra parte da decisão
+            self.actor.occupied()
         else:
             self.actor.available()
 
+
 if __name__ == '__main__':
-    r=Agent()
+    r = Agent()
     r.work()
